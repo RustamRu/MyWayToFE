69db6616b9e29f9907e3f1136d25b3249c5b614f
RustamRu <kagapov.rustam@gmail.com>
2021-11-11 00:59:45 +0300

    13-01 Адаптивная верстка

--- js/main.js
+++ js/main.js
@@ -5,3 +5,28 @@ $(document).ready(function () {
         dots: false
     });
 });
+
+function showHeaderMenu() {
+    const headermenu = document.getElementById("nav-wrapper");
+    headermenu.style.display = "block";
+    
+    const viewportcover = document.getElementById("viewport-cover");
+    viewportcover.style.display = "block";
+    
+    const burgermenu = document.getElementById("burger-menu");
+    burgermenu.style.display = "none";
+    
+    document.body.style.overflowY = "hidden";
+}
+function hideHeaderMenu() {
+    const headermenu = document.getElementById("nav-wrapper");
+    headermenu.style.display = "none";
+    
+    const viewportcover = document.getElementById("viewport-cover");
+    viewportcover.style.display = "none";
+    
+    const burgermenu = document.getElementById("burger-menu");
+    burgermenu.style.display = "block";
+
+    document.body.style.overflowY = "visible";
+}
