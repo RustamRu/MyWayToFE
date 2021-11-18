b2aeb6dc6f2851db1bec58c84ae1b1c70fdc27a8
RustamRu <kagapov.rustam@gmail.com>
2021-11-18 21:55:07 +0300

    18-04 movies-preview (sync, Timeout, Map)

--- js/movies-preview.js
+++ js/movies-preview.js
@@ -5,7 +5,7 @@ moviesContainer.innerHTML = 'Загрузка превью фильмов ...';
 const getKinopoiskAPIData = (url) => {
   return fetch(url, {
     headers: {
-      'X-API-KEY': 'e6f0bb4e-a9a2-4b83-abb1-ac92a00a66b2',
+      'X-API-KEY': API_TOKEN,
       accept: 'application/json'
     }
   })
@@ -18,50 +18,65 @@ const getFilmDetails = (id) => {
   return getKinopoiskAPIData(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`);
 }
 
+const renderFilmBlock = (film, filmData) => {
+  const filmDescId = `movies-item-desc-${film.filmId}`;
+  return `
+            <div class="movies-item movies__movies-item">
+              <a
+                href="${filmData.webUrl}"
+                target="_blank"
+                title="${film.nameRu}"
+              >
+                <img
+                  src="${film.posterUrlPreview}"
+                  alt='Постер фильма "${film.nameRu}"'
+                  class="movies-img movies__movies-img"
+                />
+                <div class="movies-item-cover movies__movies-item-cover"></div>
+                <div class="movies-item-desc movies__movies-item-desc" id="${filmDescId}">
+                  <h3 class="movies-item-desc-h3 movies__movies-item-desc-h3">
+                  ${film.nameRu}
+                  </h3>
+                  <p class="movies-item-desc-p">${filmData.description}
+                  </p>
+                </div>
+              </a>
+            </div>
+            `;
+}
+
 const getBlockFilmsData = async () => {
   try {
     const answer = await getTopFilms();
     const data = await answer.json();
+    const filmsLayout = new Map();
 
     const delay = (ms = 1) => new Promise(r => setTimeout(r, ms));
 
     const getDataSeries = async moviesfunc => {
-      let results = '';
-      for (let i = 0; i < 9; i++) {
-        let film = data.films[i];
-        await delay();
 
-        const filmDescId = `movies-item-desc-${film.filmId}`;
+      for (let film of data.films) {
+        await delay();
         const answer = await getFilmDetails(film.filmId);
         const filmData = await answer.json();
-        results += `
-        <div class="movies-item movies__movies-item">
-          <a
-            href="${filmData.webUrl}"
-            target="_blank"
-            title="${film.nameRu}"
-          >
-            <img
-              src="${film.posterUrlPreview}"
-              alt='Постер фильма "${film.nameRu}"'
-              class="movies-img movies__movies-img"
-            />
-            <div class="movies-item-cover movies__movies-item-cover"></div>
-            <div class="movies-item-desc movies__movies-item-desc" id="${filmDescId}">
-              <h3 class="movies-item-desc-h3 movies__movies-item-desc-h3">
-              ${film.nameRu}
-              </h3>
-              <p class="movies-item-desc-p">${filmData.description}
-              </p>
-            </div>
-          </a>
-        </div>
-        `;
+
+        const filmBlock = renderFilmBlock(film, filmData);
+        filmsLayout.set(film.filmId, filmBlock);
       }
-      moviesContainer.innerHTML = results;
-      return;
+
+      let i = 0;
+      let moviesContainerHTML = '';
+
+      for (const [,filmLayout] of filmsLayout) {
+        moviesContainerHTML += filmLayout;
+
+        i++;
+        if (i == 9) { break; }
+      }
+
+      moviesContainer.innerHTML = moviesContainerHTML;
     }
-    getDataSeries();
+    getDataSeries()
   } catch (error) {
     console.error(error);
   }
