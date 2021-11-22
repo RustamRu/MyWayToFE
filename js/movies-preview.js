57548898fcebb232d6cbddb1e11f11dd0130e02e
RustamRu <kagapov.rustam@gmail.com>
2021-11-22 15:12:13 +0300

    18-05 movies-preview (promise, promise.all)

--- js/movies-preview.js
+++ js/movies-preview.js
@@ -1,22 +1,4 @@
-const API_TOKEN = 'e6f0bb4e-a9a2-4b83-abb1-ac92a00a66b2';
-const moviesContainer = document.getElementById('movies-container');
 moviesContainer.innerHTML = 'Загрузка превью фильмов ...';
-
-const getKinopoiskAPIData = (url) => {
-  return fetch(url, {
-    headers: {
-      'X-API-KEY': API_TOKEN,
-      accept: 'application/json'
-    }
-  })
-}
-
-const getTopFilms = () => {
-  return getKinopoiskAPIData('https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=1');
-}
-const getFilmDetails = (id) => {
-  return getKinopoiskAPIData(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}`);
-}
 
 const renderFilmBlock = (film, filmData) => {
   const filmDescId = `movies-item-desc-${film.filmId}`;
@@ -25,7 +7,7 @@ const renderFilmBlock = (film, filmData) => {
   movieItem.classList.add('movies-item', 'movies__movies-item');
 
   const webLink = document.createElement('a');
-  webLink.href = filmData.webUrl;
+  webLink.href = `/single/?id=${film.filmId}`;
   webLink.target = '_blank';
   webLink.title = film.nameRu;
   movieItem.append(webLink);
@@ -62,34 +44,35 @@ const getBlockFilmsData = async () => {
   try {
     const answer = await getTopFilms();
     const data = await answer.json();
+    const requests = [];
     const filmsLayout = new Map();
 
-    const getDataSeries = async () => {
-      for (let film of data.films) {
-        try {
+    for (let film of data.films) {
+      const getFilmData = new Promise((resolve) => {
+        setTimeout(async () => {
           const answer = await getFilmDetails(film.filmId);
           const filmData = await answer.json();
 
           const filmBlock = renderFilmBlock(film, filmData);
           filmsLayout.set(film.filmId, filmBlock);
-        } catch (error) {
-          console.error(error);
-        }
-      }
+          resolve(filmsLayout);
+        }, requests.length * 40);
+      });
 
-      let i = 0;
-      let moviesContainerHTML = '';
+      requests.push(getFilmData);
 
-      for (const [, filmLayout] of filmsLayout) {
-        moviesContainerHTML += filmLayout;
-
-        i++;
-        if (i == 9) { break; }
-      }
-
-      moviesContainer.innerHTML = moviesContainerHTML;
+      if (requests.length == 9) { break; }
     }
-    await getDataSeries();
+
+    await Promise.all(requests);
+
+    let moviesContainerHTML = '';
+
+    for (const [, filmLayout] of filmsLayout) {
+      moviesContainerHTML += filmLayout;
+    }
+
+    moviesContainer.innerHTML = moviesContainerHTML;
   } catch (error) {
     console.error(error);
   }
